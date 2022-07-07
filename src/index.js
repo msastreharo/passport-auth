3ab5650c82ac2a5941224bdbf40520ad25e27383
Marta <msastreharo@gmail.com>
2022-07-07 09:28:17 +0200

    Commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { loginRouter } from '../routes/login.js';
+import passport from 'passport';
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use("/auth", loginRouter);
+app.use(passport.initialize());
 
 app.listen(3000, () => console.log("http://localhost:3000"));
