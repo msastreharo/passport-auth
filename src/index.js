7f169dbed1d76f7adabd6d67510075c1a6923123
Marta <msastreharo@gmail.com>
2022-07-07 09:21:41 +0200

    Added google auth

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import { loginRouter } from '../routes/login.js';
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
+app.use("/auth", loginRouter);
 
 app.listen(3000, () => console.log("http://localhost:3000"));
